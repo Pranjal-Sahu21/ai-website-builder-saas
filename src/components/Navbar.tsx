6eff2226598707791b73d256c8f3795dd13fd29a
Pranjal-Sahu21 <sahupranjal1619@gmail.com>
2026-03-27 02:43:16 +0530

    Added slide in animation in Navbar, also added Loader when credits are loading

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,18 +6,24 @@ import { authClient } from "@/lib/auth-client";
 import { UserButton } from "@daveyplate/better-auth-ui";
 import api from "@/configs/axios.config";
 import { toast } from "sonner";
+import { Loader2Icon } from "lucide-react";
+import { motion } from "framer-motion";
+
 const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState<boolean>(false);
+  const [credits, setCredits] = useState(0);
+  const [loadingCredits, setLoadingCredits] = useState(false);
+
   const scrollToSection = useScrollToSection();
 
   const { data: session } = authClient.useSession();
 
-  const [credits, setCredits] = useState(0);
-
   // Get credits function
   const getCredits = async () => {
     try {
+      setLoadingCredits(true);
       const { data } = await api.get("/api/user/credits");
+      setLoadingCredits(false);
       setCredits(data?.credits);
     } catch (error: any) {
       toast.error(error?.response?.data?.message || error.message);
@@ -25,6 +31,7 @@ const Navbar = () => {
     }
   };
 
+  // Get credits is user logged in on mount
   useEffect(() => {
     if (session?.user) {
       getCredits();
@@ -32,18 +39,22 @@ const Navbar = () => {
   }, [session?.user]);
 
   return (
-    <nav
+    <motion.nav
+      initial={{ y: -80, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ duration: 0.7, ease: "easeOut" }}
       className="fixed top-0 z-20 w-full h-20 
-        flex flex-col items-center justify-center
-        bg-black/10 backdrop-blur-3xl  
-        "
+      flex flex-col items-center justify-center
+      bg-black/10 backdrop-blur-3xl"
     >
       <div className="flex items-center justify-between p-4 md:px-16 lg:px-24 xl:px-32 md:py-4 w-full">
+        {/* Logo */}
         <Link to="/" className="text-xl text-white flex items-center gap-2">
           <img src={favicon} alt="logo" className="h-8 w-8" /> Genixor
         </Link>
 
         {session ? (
+          // If user logged in
           <div
             className={`max-lg:fixed max-lg:top-0 max-lg:z-10 max-lg:left-0 max-lg:transition-all max-lg:duration-300 max-lg:overflow-hidden max-lg:h-screen max-lg:bg-black/95 max-lg:backdrop-blur-3xl max-lg:flex-col max-lg:justify-center flex items-center gap-8 text-2xl md:text-4xl md:gap-12 lg:text-sm ${
               menuOpen ? "max-lg:w-full" : "max-lg:w-0"
@@ -87,7 +98,14 @@ const Navbar = () => {
             {session ? (
               <div className="flex gap-4 justify-center items-center">
                 <span className="text-white/80 gap-1 hidden lg:flex">
-                  Credits: <span className="text-[#a6ff5d]">{credits}</span>
+                  Credits:{" "}
+                  <span className="text-[#a6ff5d]">
+                    {loadingCredits ? (
+                      <Loader2Icon className="size-4 animate-spin" />
+                    ) : (
+                      credits
+                    )}
+                  </span>
                 </span>
                 <UserButton className="hidden lg:flex" size="icon" />
               </div>
@@ -101,6 +119,7 @@ const Navbar = () => {
             )}
           </div>
         ) : (
+          // If user not logged in
           <div
             className={`max-lg:fixed max-lg:top-0 max-lg:z-10 max-lg:left-0 max-lg:transition-all max-lg:duration-300 max-lg:overflow-hidden max-lg:h-screen max-lg:bg-black/95 max-lg:backdrop-blur-3xl max-lg:flex-col max-lg:justify-center flex items-center gap-8 text-2xl md:text-4xl md:gap-12 lg:text-sm ${
               menuOpen ? "max-lg:w-full" : "max-lg:w-0"
@@ -167,13 +186,26 @@ const Navbar = () => {
 
         <div className="flex -mr-2 justify-center items-center lg:hidden md:gap-2">
           {session ? (
+
+            // Credits and user button
             <div className="flex justify-center items-center">
-              <span className="text-white/80 p-3 rounded-full text-sm">
-                Credits: <span className="text-[#a6ff5d]">{credits}</span>
+              <span className="text-white/80 p-3 rounded-full text-sm flex items-center justify-center gap-1">
+                Credits:
+                <span className="text-[#a6ff5d]">
+                  {loadingCredits ? (
+                    <Loader2Icon className="size-4 animate-spin" />
+                  ) : (
+                    credits
+                  )}
+                </span>
               </span>
+
+              {/* User button */}
               <UserButton className="flex lg:hidden" size="icon" />
             </div>
           ) : (
+
+            // Login button
             <Link
               to="/auth/sign-in"
               className=" lg:flex border bg-[#A6FF5D] hover:bg-[#A6FF5D]/90 text-gray-900 px-6 py-2 text-sm rounded-full transition"
@@ -182,6 +214,8 @@ const Navbar = () => {
             </Link>
           )}
 
+
+          {/* Hamburger menu */}
           <div className="md:fixed md:top-5 md:right-2 z-50">
             <button
               onClick={() => setMenuOpen(true)}
@@ -198,7 +232,7 @@ const Navbar = () => {
           </div>
         </div>
       </div>
-    </nav>
+    </motion.nav>
   );
 };
 
